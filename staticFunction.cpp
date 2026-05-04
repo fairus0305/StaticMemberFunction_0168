e4b65b1ec7120608721adf17e45c9b764b2d65b2
fairus0305 <ruszezzy@gmail.com>
2026-05-04 15:49:43 +0700

    menambahkan method printAll pada class Mahasiswa

--- staticFunction.cpp
+++ staticFunction.cpp
@@ -34,7 +34,12 @@ public:
 int Mahasiswa::nim = 0;
 
 // pendefinisial prosedur - prosedur diluar class
-void Mahasiswa::setID()
-{
+void Mahasiswa::setID(){
     id = ++nim;
+}
+
+void Mahasiswa::printAll(){
+    cout << "ID = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
 }
