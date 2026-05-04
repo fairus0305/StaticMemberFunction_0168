44e8f05d637cd9e64dc1a8e9d74344eb3784a13d
fairus0305 <ruszezzy@gmail.com>
2026-05-04 15:31:40 +0700

    Menambahkan implementasi fungsi setID

--- staticmember.cpp
+++ staticmember.cpp
@@ -22,3 +22,9 @@ public:
 
 // memberi value ke variabel nim dari class mahasiswa
 int Mahasiswa::nim = 1;
+
+// mengimplementasikan prosedur - prosedur di luar class
+void Mahasiswa::setID()
+{
+    id = ++nim;
+};
