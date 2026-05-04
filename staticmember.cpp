b8b9ec0e09d3bdc83e289e3e14f34c5b1b4dff9d
fairus0305 <ruszezzy@gmail.com>
2026-05-04 15:28:21 +0700

    Menambahkan deklarasi prosedur setID dan printAll

--- staticmember.cpp
+++ staticmember.cpp
@@ -2,10 +2,14 @@
 using namespace std;
 
 class Mahasiswa {
-    
+
 public:
     static int nim; // deklarasi variabel static yaitu variabel nim
     // deklarasi variabel member
     int id;
     string nama;
+
+    // deklarasi prosedur setID() dan printAll()
+    void setID();
+    void printAll();
 }
